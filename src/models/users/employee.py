1500260f115392a685f28dd202f6f89916e922b4
bruce <shenchen25@hotmail.com>
2016-04-17 16:17:04 +0800

    User management

--- src/models/users/employee.py
+++ src/models/users/employee.py
@@ -4,21 +4,70 @@ Created on Apr 14, 2016
 @author: Bruce
 '''
 from src.models.database import get_session
-from src.models.users.models import Employee
+from src.models.users.models import Employee,Member
+import datetime
 
-
-def employeeQuery(name=None,phone=None,webChat=None):
+def employeeQuery(name=None,phone=None,webChat=None,offset=0,limit=10):
+    """
+    Query employee by name,phone,wehchat. it could be used for page when using offset,limit. Default is return first 10
+    """
     seesion=get_session()
     querys=seesion.query(Employee)
     if name is not None:
-        querys=querys.filter(Employee.name==name)
+        querys=querys.filter(Employee.name.like("%"+name+"%"))
     if phone is not None:
-        querys=querys.filter(Employee.phone==phone)
+        querys=querys.filter(Employee.phone.like(phone+"%"))
     if webChat is not None:
-        querys=querys.filter(Employee.name==webChat)
+        querys=querys.filter(Employee.wchat.like("%"+webChat+"%"))
+    querys=querys.offset(offset)  
+    querys=querys.limit(limit)
     return querys.all()
+
+def employeeAdd(name,phone,webChat=None,birthday=None,address=None,password="changeme",**args):
+    """
+    Add Employee, the default password is 'changeme'. At the first login, we need to force change password
+    """
+    employee=Employee()
+    employee.name=name
+    employee.phone=phone
+    employee.password=password
+    if not webChat is None:
+        employee.phone=webChat
+    if not birthday is None:
+        employee.phone=birthday
+    if not address is None:
+        employee.phone=address
+        
+    for item in args:
+        if hasattr(employee, item):
+            employee=args[item]
+        else:
+            raise "Employee Didn't have this property: " + item
+
     
+if __name__ == '__main__':
+    import sys
+    from src.models import database,UserProfile,UserStatus
+    from datetime import datetime
+#     database.drop_database()
+#     database.create_database()
+    session = database.get_session()
+
     
-    
-    
+#     i=0
+#     while i <200:
+#         print i
+#         user1 = Employee()  
+#         user1.name = 'bruce'+str(i)
+#         user1.phone = u'189'+str(i)
+#         user1.wchat = 'test'+str(i)
+#         user1.birthday = datetime.now()
+#         i=i+1
+#         session.add(user1)
+#         session.commit()
+#     session.close()    
+    results= employeeQuery('bruce','189',offset=10,limit=20)
+    for item in results:
+        print item.birthday
+    print dir(Employee)
         
--- src/models/users/models.py
+++ src/models/users/models.py
@@ -4,7 +4,12 @@ Created on Apr 14, 2016
 @author: Bruce
 '''
 from src.models.database import BaseModel
+
 from sqlalchemy import Column, Integer, String,Date,Sequence
+from sqlalchemy.orm import relationship
+from sqlalchemy.schema import ForeignKey
+from sqlalchemy import *
+
 
 class Employee(BaseModel):
     '''
@@ -12,13 +17,47 @@ class Employee(BaseModel):
     '''
     
     __tablename__ = 'employees'
-    id = Column(Integer,Sequence('user_id_seq'), primary_key=True)
-    name = Column(String(50))
-    phone = Column(String(11))
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50),nullable=False)
+    phone = Column(String(11),nullable=False)
     wchat = Column(String(30))
     address = Column(String(120))
     birthday = Column(Date())
-    password= Column(String(20))
+    password= Column(String(20),nullable=False)
+    role=Column(Enum('coach','manger','front','other',))
+    
+    coach_Info = relationship("Member",primaryjoin="Employee.id==Member.coach_id",backref="employees")
+
+class bodyMeasurement(BaseModel):
+    __tablename__ = 'body'
+    id = Column(Integer, primary_key=True)
+    
+    body_Info = relationship("Member",primaryjoin="bodyMeasurement.id==Member.body_id",backref="body")
+
+    
+class Member(BaseModel):
+    '''
+    Member model
+    '''
+    
+    __tablename__ = 'members'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50),nullable=False)
+    phone = Column(String(11),nullable=False)
+    wchat = Column(String(30))
+    type=Column(Enum('vip','normal'))
+    birthday = Column(Date())
+    expenses_not_quota_password= Column(String(20),nullable=False)
+    expenses_quota=Column(Integer)
+    expenses_not_quota=Column(Integer)
+    course_num = Column(Integer)
+    course_num_free = Column(Integer)
+    
+    body_id=Column(Integer, ForeignKey('body.id'))
+    
+    coach_id=Column(Integer, ForeignKey('employees.id'))
+    
+    
 
 
     
