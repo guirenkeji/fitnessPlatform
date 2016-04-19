04ba6e45783313729274fc5b5f8e37f212f1e4a2
pingyifen <irelessqa.me@gmail.com>
2016-04-19 08:51:50 +0800

    static views

---
+++ src/static/js/controller/user.js
@@ -0,0 +1,14 @@
+dashboard.controller('userManagement', ['$scope', function($scope){
+
+	$scope.init = function () {
+		// App.init();
+	}
+}])
+
+
+dashboard.controller('userAdd', ['$scope', function($scope){
+
+	$scope.init = function () {
+		// App.init();
+	}
+}])
