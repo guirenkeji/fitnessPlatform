b6db2f8ce8d9d6f626cf8da8321f3657d51e98c6
fls <irelessqa.me@gmail.com>
2016-04-19 09:11:07 +0800

    合并controller

--- src/static/js/controller.js
+++ src/static/js/controller.js
@@ -5,3 +5,30 @@ dashboard.controller('sidebar', ['$scope', '$rootScope', function($scope, $rootS
 		console.log($rootScope.path);
 	}
 }])
+dashboard.controller('LoginCtrl', ['$scope','$http', function($scope,$http){
+	    $scope.isMatch = true;
+	    $scope.isDisabled = false;
+	    $scope.login = function () 
+	    {
+	        $scope.isMatch = true;
+	        $scope.isDisabled = false;
+	        var btn = $("#btnLogin");
+	        btn.button('loading');
+	        $http.post('/Login', $scope.User).success(function (result) 
+	        {
+	            btn.button('reset');
+	            if (result.isMatch != null) 
+	            {
+	                $scope.isMatch = result.isMatch;
+	            }
+	            if (result.isDisabled != null) 
+	            {
+	                $scope.isDisabled = result.isDisabled;
+	            }
+	            if (result.isMatch != null && result.isMatch) 
+	            {
+	                window.location.href = '/fitnessmanages';
+	            }
+	        });
+	    };
+	}])
