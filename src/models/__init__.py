dfbfbb793d54863fad5f0c7faf22799e6de0dc7d
fls <irelessqa.me@gmail.com>
2016-04-21 19:37:55 +0800

    del model member

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -2,4 +2,3 @@
 from src.models.userprofile import UserProfile,UserStatus
 from src.models.course import Course,Plan,TypeCode
 from src.models.venue import Venue,Yard,UsageStatus
-from src.models.member import Member
