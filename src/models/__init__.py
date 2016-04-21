46546a6384f189589c45427ecd3ac65068f3878d
fls <irelessqa.me@gmail.com>
2016-04-21 21:02:03 +0800

    修改了course relationship  #注释暂时注销了yard coach和venue models relationship相关内容

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -2,3 +2,5 @@
 from src.models.userprofile import UserProfile,UserStatus
 from src.models.course import Course,Plan,TypeCode
 from src.models.venue import Venue,Yard,UsageStatus
+from src.models.member import Member
+from src.models.employee import Employee
