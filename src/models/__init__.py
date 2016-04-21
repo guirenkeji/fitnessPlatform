bd4d52087cfc5cf38014d763489f9c421fb6679a
rsh <rsh735@126.com>
2016-04-22 01:39:19 +0800

    order.py

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -2,4 +2,5 @@
 from src.models.userprofile import UserProfile,UserStatus
 from src.models.course import Course,Plan,TypeCode
 from src.models.venue import Venue,Yard,UsageStatus
-from src.models.member import Member
+from src.models.order import Order,Goods,OrderStatus,PayMode,OrderType
+from src.models.users.models import Member
