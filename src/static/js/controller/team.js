889f342eba1ba613c1b392d2c9cbdffcf9a8afbf
bruce <shenchen25@hotmail.com>
2016-04-26 20:37:14 +0800

    js debug

--- src/static/js/controller/team.js
+++ src/static/js/controller/team.js
@@ -7,17 +7,15 @@ dashboard.controller('teamCoursesManagement', ['$scope', function($scope){
 
 dashboard.controller('teamCoursesAdd', ['$scope','$http', function($scope,$http){
     $scope.create = function () {
-        var btn = $("#btnCreate");
-        btn.button('loading');
+        
         $scope.Course = {};
         $scope.Course.fullname = $("#fullname").val();
         $scope.Course.dataclasstime = $("#data-class-time").val();
         $scope.Course.message = $("#message").val();
         $http.post('/courses/team/management/add', $scope.Course,$http).success(function (result) {
         	if (result.created) {
-                $scope.AddSuccess = true;
-                btn.button('reset');
-                window.location.href = "/Project/Task/" + result.ProjectId;
+                alert("Success");
+                
             }
         });
     }
--- src/static/js/controller/user.js
+++ src/static/js/controller/user.js
@@ -2,13 +2,33 @@ dashboard.controller('userManagement', ['$scope', function($scope){
 
 	$scope.init = function () {
 		// App.init();
-	}
+	};
+	$scope.formData = {};
 }])
 
 
-dashboard.controller('userAdd', ['$scope', function($scope){
+dashboard.controller('memberAdd', ['$scope', function($scope){
 
 	$scope.init = function () {
 		// App.init();
 	}
+	$scope.create = function () {
+        var btn = $("#btnCreate");
+        btn.button('loading');
+		$scope.Course = {};
+        $scope.Course.name = $("#fullname").val();
+        $scope.Course.birthday = $("#data-class-time").val();
+		$scope.Course.sex=$("#radiorequired").val();
+		$scope.Course.phone=$("#data-phone").val();
+		$scope.Course.address=$("#data-address").val();
+        $scope.Course.wchat=$("#data-wchat").val();
+        $scope.Course.comments = $("#message").val();
+        $http.post('/fitnessmanages/addMember', $scope.Course).success(function (result) {
+        	if (result.created) {
+                $scope.AddSuccess = true;
+                btn.button('reset');
+                window.location.href = "/Project/Task/" + result.ProjectId;
+            }
+        });
+    }
 }])
