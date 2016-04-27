1fba2975a31248be2409f8b95f8f38dfad07cb92
fls <irelessqa.me@gmail.com>
2016-04-27 11:28:01 +0800

    添加了course.js manage的 deleted update 方法结构

--- src/static/js/controller/course.js
+++ src/static/js/controller/course.js
@@ -12,6 +12,44 @@ dashboard.controller('teamCoursesManagement', ['$scope', '$http', function($scop
 	    		self.courselist = result.data;
 	        }
 	    }).error(function (data, status, headers, config) { });
+		
+		
+		$scope.Delete = function () {
+	        var btn = $("#btnDelete");
+	        btn.button('loading');
+	        $http.post('/Delete', { CourseId: $scope.Course.id }).success(function (result) {
+	            if (result.deleted) {
+	                $scope.DeleteSuccess = true;
+	                btn.button('reset');
+	                window.location.href = 'fitnessmanages#/courses/team/management';
+	            }
+	        });
+	    }
+	    $scope.update = function () {
+	        var btn = $("#btnUpdate");
+	        btn.button('loading');
+	        $http.post('/Update', $scope.Course).success(function (result) {
+	            if (result.updated) {
+	                $scope.UpdateSuccess = true;
+	                btn.button('reset');
+	                window.location.href = 'fitnessmanages#/courses/team/management';
+	            }
+	        });
+	    }
+	    $scope.query = function () {
+	        var btn = $("#btnQuery");
+	        btn.button('loading');
+	        $http.post('/query', $scope.Query).success(function (result) {
+	            btn.button('reset');
+	            $scope.Curse = result.data;
+	            $scope.Query.RowCount = result.row_count;
+	            $scope.Query.PageCount = result.page_count;
+	            $scope.Query.PageNo = result.page_no;
+	        });
+	    }    
+
+		
+		
 	
 	    $scope.init = function () {
 			// App.init();
