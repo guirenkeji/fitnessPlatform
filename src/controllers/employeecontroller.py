4c52064cff11da8c7b8ef5f8919199d6f74adaee
bruce <shenchen25@hotmail.com>
2016-04-28 00:57:16 +0800

    js debug

---
+++ src/controllers/employeecontroller.py
@@ -0,0 +1,73 @@
+# -*- coding: UTF-8 -*- 
+import json
+
+from flask import Module,render_template,jsonify, redirect, request,session,g
+from src.fitnessconfig import *
+from src.services import userservice,employeeservice,memberservice
+from src.models.userprofile import UserStatus
+import datetime
+employeemanages = Module(__name__)
+
+
+def date_handler(obj):
+    return obj.isoformat() if hasattr(obj, 'isoformat') else obj
+
+@employeemanages.route('/fitnessmanages/addEmployee',methods=["POST"])
+def addNewEmployee():
+      
+    addOne= request.get_json()
+    print "***"*20
+    import pprint
+    pprint.pprint(addOne)
+    name = addOne.get('name')
+    comments=addOne.get('comments',None)
+    phone = addOne.get('phone')
+    wchat = addOne.get('wchat',None)
+    birthday = addOne.get('birthday',None)
+    if not birthday is None:
+        birthday=datetime.datetime.strptime(birthday, '%m/%d/%Y').date() 
+    address = addOne.get('address',None)
+    sex = addOne.get('sex','man')
+    employeeservice.employeeAdd(name=name,phone=phone,webChat=wchat,birthday=birthday,address=address,sex=sex)
+    return jsonify(created=True)
+
+
+@employeemanages.route('/fitnessmanages/modifyEmployee',methods=["GET"])
+def modifyEmployee():
+    argDict=request.args.to_dict()
+    employeeservice.employeeModify(argDict.pop('id'),**argDict)
+    
+    return redirect('/fitnessmanages?fullname=&message=#/personnel/management/add')  
+
+
+@employeemanages.route('/fitnessmanages/searchEmployee',methods=["POST"])
+def searchEmployee():
+    key = request.json['searchKey']
+    results=[]
+    employees=employeeservice.employeeFuzzyQuery(key)
+    for employee in employees:
+        bd =employee.birthday
+        if not bd is None:
+            bd=bd.strftime('%m/%d/%Y')
+        if employee.sex == 'man':
+            sex='男'
+        else:
+            sex='女'
+        results.append({'id':employee.id,'name':employee.name,'phone':employee.phone,'wchat':employee.wchat,'birthday':bd,'address':employee.address,'sex':sex,'taozhang':'CN'})
+    
+
+    return jsonify(data=results,row_count=len(employees),page_count=1,page_no=request.json['PageNo'])
+
+
+@employeemanages.route('/fitnessmanages/getEmployee')
+def getEmployeeByID():
+    eid = request.args.get('id')
+    employ=employeeservice.employeeGetByID(eid)
+    info={}
+    for key in employ.__dict__:
+        if not key.startswith("_"):
+            info[key]=employ.__dict__[key]
+                
+
+    return json.dumps(info, default=date_handler)
+
--- src/static/js/controller/user.js
+++ src/static/js/controller/user.js
@@ -16,19 +16,54 @@ dashboard.controller('userAdd', ['$scope', function($scope){
 }])
 
 // 人事管理
-dashboard.controller('personnelManagement', ['$scope', function($scope){
+dashboard.controller('personnelManagement', ['$scope', '$http', function($scope,$http){
 
 	$scope.init = function () {
 		// App.init();
 	}
+	
+	var self = this;
+	$scope.Page = {};
+	if($("#PageNo").val()){
+		$scope.Page.PageNo = $("#PageNo").val();
+	}else{
+		$scope.Page.PageNo = 1;
+	}
+	$scope.Page.searchKey='';
+	$http.post('/fitnessmanages/searchEmployee', $scope.Page, $http).success(function (result) {
+    	if (result.data) {
+    		$scope.employeelist = result.data;
+    		
+        }
+    }).error(function (data, status, headers, config) { });
 }])
 
 
-dashboard.controller('personnelAdd', ['$scope', function($scope){
+dashboard.controller('personnelAdd',['$scope','$http', function($scope,$http){
 
 	$scope.init = function () {
 		// App.init();
+		$scope.formdata = {};
 	}
+
+	$scope.create = function () {
+        
+		 var btn = $("#btnCreate");
+	     btn.button('loading');
+	        
+        $http({
+          method  : 'POST',
+          url     : '/fitnessmanages/addEmployee',
+          data    : $scope.formdata, 
+          headers : {'Content-Type': 'application/json'} 
+         }).success(function (result) {
+        	 if (result.created) {
+                 $scope.AddSuccess = true;
+                 btn.button('reset');
+                 window.location.href = "fitnessmanages#/personnel/management";
+             }
+        });
+    }
 }])
 
 // 会员管理
@@ -50,16 +85,42 @@ dashboard.controller('memberAdd',['$scope','$http', function($scope,$http){
 
 	$scope.create = function () {
         
-		
+		 var btn = $("#btnCreate");
+	     btn.button('loading');
+	        
         $http({
           method  : 'POST',
           url     : '/fitnessmanages/addMember',
           data    : $scope.formdata, 
-          headers : {'Content-Type': 'application/x-www-form-urlencoded'} 
+          headers : {'Content-Type': 'application/json'} 
          }).success(function (result) {
-        	if (result.created) {
-                $scope.AddSuccess = true;
-            }
+        	 if (result.created) {
+                 $scope.AddSuccess = true;
+                 btn.button('reset');
+                 window.location.href = "fitnessmanages#/member/management";
+             }
         });
     }
+}])
+
+dashboard.controller('memberManagement', ['$scope', '$http', function($scope,$http){
+		var self = this;
+		$scope.Page = {};
+		if($("#PageNo").val()){
+			$scope.Page.PageNo = $("#PageNo").val();
+		}else{
+			$scope.Page.PageNo = 1;
+		}
+		$scope.Page.searchKey='';
+		$http.post('/fitnessmanages/searchMember', $scope.Page, $http).success(function (result) {
+	    	if (result.data) {
+	    		$scope.memberlist = result.data;
+	    		
+	        }
+	    }).error(function (data, status, headers, config) { });
+		
+	
+	    $scope.init = function () {
+			// App.init();
+	    }
 }])
